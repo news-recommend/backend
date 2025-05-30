ec45ce330f331d127230233da46926e3f159f93c
mayrang <pkss0626@naver.com>
2025-05-30 16:44:16 +0900

    cors 설정 추가

---
+++ src/main/java/news_recommend/news/config/CorsConfig.java
@@ -0,0 +1,17 @@
+package news_recommend.news.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowCredentials(false)
+                .allowedHeaders("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+    }
+}
