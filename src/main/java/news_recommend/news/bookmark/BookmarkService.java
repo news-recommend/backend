66850ad71ddb1f46b101fc0a92787e4d52aede54
GHS <20221003@edu.hanbat.ac.kr>
2025-06-13 18:58:10 +0900

    북마크 응답에 이슈명 및 카테고리 포함, APIResponse 적용

--- src/main/java/news_recommend/news/bookmark/BookmarkService.java
+++ src/main/java/news_recommend/news/bookmark/BookmarkService.java
@@ -6,35 +6,8 @@ import news_recommend.news.bookmark.repository.BookmarkRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
-
-//public class BookmarkService {
-//
-//    private final BookmarkRepository bookmarkRepository;
-//
-//    public BookmarkService(BookmarkRepository bookmarkRepository) {
-//        this.bookmarkRepository = bookmarkRepository;
-//    }
-//
-//    public Bookmark addBookmark(Bookmark bookmark) {
-//        return bookmarkRepository.save(bookmark);
-//    }
-//
-//    public void deleteByUserAndIssue(Long userId, Long issueId) {
-//        bookmarkRepository.deleteByUserAndIssue(userId, issueId);
-//    }
-//
-//    public List<Bookmark> getBookmarksByUserId(Long userId) {
-//        return bookmarkRepository.findByUserId(userId);
-//    }
-//
-//    // 자세한 북마크 리스트 출력
-//    public List<BookmarkResponseDto> getDetailedBookmarksByUserId(Long userId) {
-//        return bookmarkRepository.findDetailedByUserId(userId);
-//    }
-//}
 
 // JWT
 
--- src/main/java/news_recommend/news/bookmark/repository/BookmarkRepository.java
+++ src/main/java/news_recommend/news/bookmark/repository/BookmarkRepository.java
@@ -1,22 +1,9 @@
 package news_recommend.news.bookmark.repository;
 
 import news_recommend.news.bookmark.Bookmark;
-import news_recommend.news.bookmark.dto.BookmarkResponseDto;
 
 import java.util.List;
-import java.util.Optional;
 
-//public interface BookmarkRepository {
-//
-//    Bookmark save(Bookmark bookmark);
-//
-//    void deleteByUserAndIssue(Long userId, Long issueId);
-//
-//    List<Bookmark> findByUserId(Long userId); // 내 북마크 리스트
-//
-//    // 자세한 북마크 정보 출력
-//    List<BookmarkResponseDto> findDetailedByUserId(Long userId);
-//}
 
 // JWT
 
